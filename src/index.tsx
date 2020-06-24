9eff31f11b65c79927069143e26f0bd6bba09912
luck_yan_0801@163.com <luck_yan_0801@163.com>
2020-06-24 18:22:04 +0800

    代码fei   分页代码f分片整理

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,6 @@
 import * as React from 'react'
 import * as ReactDom from 'react-dom'
 import * as styles from './index.scss'
-import Index from './pages/index/index'
+import App from './pages/index/index'
 
-function getComponent() {
-    return import(/* webpackChunkName: "lodash" */ 'lodash').then(({ default: _ }) => {
-      const element = document.createElement('div');
-
-      element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-      return element;
-
-    }).catch(error => 'An error occurred while loading the component');
-}
-
-getComponent().then(component => console.log('component', component))
-
-ReactDom.render(
-    <div className={ styles.testContainer }>
-        hello world 465132
-        <div className={ styles.test2 }>
-            hello qa!!!
-        </div>
-        <Index />
-    </div>,
-document.querySelector('#App'))
+ReactDom.render( <App />, document.querySelector('#App'))
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,13 +1,15 @@
 import * as React from 'react'
 import * as styles from './index.scss'
 import Test from '@/components/Test/Test'
+import SplitCode from '@/pages/SplitCode/SplitCode'
 
-export default class Index extends React.Component {
+export default class App extends React.Component {
     render () {
         return (
             <div className={ styles.testContainer }>
                 index container
                 <Test />
+                <SplitCode />
             </div>
         )
     }
